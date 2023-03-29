1b9b49e02ad9ac1f0d68739e2c469facf450bb38
Azure7896 <azurusek@gmail.com>
2023-03-29 03:31:48 +0200

    Form changes, add validation with button disabled functionality 03.29

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { FormComponent } from './form/form.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatInputModule} from "@angular/material/input";
-import {ReactiveFormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {MatCardModule} from "@angular/material/card";
 import {MatButtonModule} from '@angular/material/button';
 import {MatDatepickerModule} from "@angular/material/datepicker";
@@ -28,7 +28,8 @@ import {HttpClientModule} from "@angular/common/http";
     MatButtonModule,
     ReactiveFormsModule,
     MatDatepickerModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
